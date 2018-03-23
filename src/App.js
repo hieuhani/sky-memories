69da9be002f637b1e1859524e03e658c37cc3a9c
Hieu Tran <658655@gmail.com>
2018-03-24 00:37:08 +0700

    Add stars to the universe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import universe from './night.jpg';
+import _ from 'lodash';
+import universe from './universe.jpg';
 
+const Wrapper = styled.div.attrs({
+  style: ({mouseX, mouseY}) => ({
+    left: `${mouseX}px`,
+    top: `${mouseY}px`,
+  }),
+})`
+position: absolute;
+right: 0;
+bottom: 0;
+overflow: hidden;
+`
 const Background = styled.div`
   background: url('${universe}');
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-position: ${({mouseX, mouseY}) => `${mouseX}px ${mouseY}px`};
+  background-repeat: no-repeat;
+  width: 3840px;
+  height: 2160px;
 `;
+
+const Point = styled.div`
+  width: 16px;
+  height: 16px;
+  background-color: transparent;
+  border-radius: 50%;
+  border: 2px solid #CCFF00;
+  position: absolute;
+  top: ${({ baseY }) => `${baseY}px`};
+  left: ${({ baseX }) => `${baseX}px`};
+
+`
+
+const pointPositions = {};
+_.times(60).forEach((time) => {
+  pointPositions[time] = {
+    baseX: _.random(0, 3840),
+    baseY: _.random(0, 2160),
+  };
+});
 
 class App extends Component {
   constructor(props) {
@@ -21,14 +50,20 @@ class App extends Component {
 
   handleMouseMove(event) {
     this.setState({
-      x: -(event.clientX / 2),
-      y: -(event.clientY / 2),
+      x: - (event.clientX / 2),
+      y: - (event.clientY / 2),
     });
   }
 
   render() {
     return (
-      <Background mouseX={this.state.x} mouseY={this.state.y} onMouseMove={this.handleMouseMove} />
+      <Wrapper mouseX={this.state.x} mouseY={this.state.y} onMouseMove={this.handleMouseMove}>
+        <Background>
+          {Object.keys(pointPositions).map((key) => (
+            <Point key={key} baseX={pointPositions[key].baseX} baseY={pointPositions[key].baseY} />
+          ))}
+        </Background>
+      </Wrapper>
     );
   }
 }
